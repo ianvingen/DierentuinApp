aee9f8103695a94cb25277e4da5e76df255e286b
ianVanIngen <ianvaning3n@gmail.com>
2026-01-07 17:01:24 +0100

    feat(model-fix): Database migrations toegevoegd

---
+++ Migrations/20260107152710_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DierentuinApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enclosures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Climate = table.Column<int>(type: "INTEGER", nullable: false),
+                    HabitatType = table.Column<int>(type: "INTEGER", nullable: false),
+                    SecurityLevel = table.Column<int>(type: "INTEGER", nullable: false),
+                    Size = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enclosures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Species = table.Column<string>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    Size = table.Column<int>(type: "INTEGER", nullable: false),
+                    DietaryClass = table.Column<int>(type: "INTEGER", nullable: false),
+                    ActivityPattern = table.Column<int>(type: "INTEGER", nullable: false),
+                    Prey = table.Column<string>(type: "TEXT", nullable: false),
+                    EnclosureId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SpaceRequirement = table.Column<double>(type: "REAL", nullable: false),
+                    SecurityRequirement = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animals_Enclosures_EnclosureId",
+                        column: x => x.EnclosureId,
+                        principalTable: "Enclosures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_EnclosureId",
+                table: "Animals",
+                column: "EnclosureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Enclosures");
+        }
+    }
+}
